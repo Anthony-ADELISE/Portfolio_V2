ca9703bf1a8955e6233b07c6d78331082088ee1d
ADELISE Anthony <anthony.adelise@gmail.com>
2022-08-11 23:45:58 +0200

    mise en place des liens actifs

--- src/components/Social.jsx
+++ src/components/Social.jsx
@@ -11,10 +11,19 @@ const Social = () => {
     <div>
       {/* icons */}
       <div className=" flex space-x-4 text-3xl justify-center cursor-pointer lg:justify-start pb-4 lg:text-3xl ">
-        <FaFacebook className="socials" />
-        <BsInstagram className="socials" />
-        <BsTwitter className="socials" />
-        <BsGithub className="socials" />
+        <a className="text-3xl" href="https://www.instagram.com/tini972/">
+          <FaFacebook className="socials" />
+        </a>
+
+        <a className="text-3xl" href="https://www.instagram.com/tini972/">
+          <BsInstagram className="socials" />
+        </a>
+        <a className="text-3xl" href="https://twitter.com/AdeliseAnthony">
+          <BsTwitter className="socials" />
+        </a>
+        <a className="text-3xl" href="https://github.com/Anthony-ADELISE">
+          <BsGithub className="socials" />
+        </a>
       </div>
     </div>
   );
